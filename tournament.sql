3df0897265c5c6b81336b2df1d6fbe5f62df7e07
Sarah Caplan <caplan.sarah@gmail.com>
2017-04-01 21:05:49 -0600

    Test data

--- tournament.sql
+++ tournament.sql
@@ -9,6 +9,8 @@ DROP DATABASE IF EXISTS tournament;
 
 CREATE DATABASE tournament;
 
+\c tournament;
+
 CREATE TABLE players (id serial PRIMARY KEY, name text UNIQUE NOT NULL, created timestamp DEFAULT current_timestamp);
 
 CREATE TABLE plays (id serial PRIMARY KEY, round integer NOT NULL, match integer UNIQUE NOT NULL);
@@ -17,4 +19,20 @@ CREATE TABLE matches (id serial PRIMARY KEY REFERENCES plays (match), player1 in
 
 CREATE TABLE playsRecord (id serial PRIMARY KEY REFERENCES plays (id), winner integer NOT NULL, loser integer NOT NULL);
 
-\c tournament;
+
+INSERT INTO players (name) VALUES ('Sarah');
+INSERT INTO players (name) VALUES ('Ida');
+INSERT INTO players (name) VALUES ('Ellen');
+INSERT INTO players (name) VALUES ('Mikey');
+
+INSERT INTO plays (round, match) VALUES (1, 1);
+INSERT INTO plays (round, match) VALUES (1, 2);
+INSERT INTO plays (round, match) VALUES (2, 3);
+INSERT INTO plays (round, match) VALUES (2, 4);
+
+INSERT INTO matches (player1, player2) VALUES (1, 2);
+INSERT INTO matches (player1, player2) VALUES (3, 4);
+INSERT INTO matches (player1, player2) VALUES (1, 3);
+INSERT INTO matches (player1, player2) VALUES (2, 4);
+
+INSERT INTO playsRecord (winner, loser) VALUES
