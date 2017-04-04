3c730ea5afb4b815248b2e46a69c83e3ce1ca91e
Sarah Caplan <caplan.sarah@gmail.com>
2017-04-04 00:54:47 -0600

    Removed table "plays"

--- tournament.sql
+++ tournament.sql
@@ -13,32 +13,32 @@ CREATE DATABASE tournament;
 
 CREATE TABLE players (id serial PRIMARY KEY, name text UNIQUE NOT NULL, created timestamp DEFAULT current_timestamp);
 
-CREATE TABLE plays (id serial PRIMARY KEY, round integer NOT NULL, match integer UNIQUE NOT NULL);
+-- CREATE TABLE plays (id PRIMARY KEY, round integer NOT NULL, match integer UNIQUE NOT NULL);
 
-CREATE TABLE matches (id serial PRIMARY KEY REFERENCES plays (match), player1 integer NOT NULL, player2 integer NOT NULL);
+CREATE TABLE matches (id serial PRIMARY KEY, player1 integer NOT NULL, player2 integer NOT NULL);
 
-CREATE TABLE playsRecord (id serial PRIMARY KEY REFERENCES plays (id), winner integer NOT NULL, loser integer NOT NULL);
+CREATE TABLE playsRecord (id serial PRIMARY KEY, winner integer NOT NULL, loser integer NOT NULL);
 
 
-INSERT INTO players (name) VALUES ('Sarah');
-INSERT INTO players (name) VALUES ('Ida');
-INSERT INTO players (name) VALUES ('Ellen');
-INSERT INTO players (name) VALUES ('Mikey');
+-- INSERT INTO players (name) VALUES ('Sarah');
+-- INSERT INTO players (name) VALUES ('Ida');
+-- INSERT INTO players (name) VALUES ('Ellen');
+-- INSERT INTO players (name) VALUES ('Mikey');
 
-INSERT INTO plays (round, match) VALUES (1, 1);
-INSERT INTO plays (round, match) VALUES (1, 2);
-INSERT INTO plays (round, match) VALUES (2, 3);
-INSERT INTO plays (round, match) VALUES (2, 4);
+-- INSERT INTO plays (round, match) VALUES (1, 1);
+-- INSERT INTO plays (round, match) VALUES (1, 2);
+-- INSERT INTO plays (round, match) VALUES (2, 3);
+-- INSERT INTO plays (round, match) VALUES (2, 4);
 
-INSERT INTO matches (player1, player2) VALUES (1, 2);
-INSERT INTO matches (player1, player2) VALUES (3, 4);
-INSERT INTO matches (player1, player2) VALUES (1, 3);
-INSERT INTO matches (player1, player2) VALUES (2, 4);
+-- INSERT INTO matches (player1, player2) VALUES (1, 2);
+-- INSERT INTO matches (player1, player2) VALUES (3, 4);
+-- INSERT INTO matches (player1, player2) VALUES (1, 3);
+-- INSERT INTO matches (player1, player2) VALUES (2, 4);
 
-INSERT INTO playsRecord (winner, loser) VALUES (1, 2);
-INSERT INTO playsRecord (winner, loser) VALUES (3, 4);
-INSERT INTO playsRecord (winner, loser) VALUES (3, 1);
-INSERT INTO playsRecord (winner, loser) VALUES (4, 2);
+-- INSERT INTO playsRecord (winner, loser) VALUES (1, 2);
+-- INSERT INTO playsRecord (winner, loser) VALUES (3, 4);
+-- INSERT INTO playsRecord (winner, loser) VALUES (3, 1);
+-- INSERT INTO playsRecord (winner, loser) VALUES (4, 2);
 
 -- Join player id from matches with player name
 -- SELECT players.id as player_id, players.name from players, matches where players.id = matches.player1;
